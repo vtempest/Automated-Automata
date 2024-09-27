c0c65c5de80ce9e45b55285e7bb62d6f9fdfb522
vtempest <you@example.com>
2024-09-26 20:15:01 -0700

    android app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vtempest.automata',
+  appName: 'Automated Automata',
+  webDir: 'build'
+};
+
+export default config;
---
+++ mobile.md
@@ -0,0 +1,6 @@
+bun i @capacitor/core
+bun i -D @capacitor/cli
+bunx cap init
+bun  i @capacitor/android 
+bun cap add android
+
